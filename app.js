5751eb414fb65d04521023fdc7336c24da6a7365
Jacob Lara <l1a9r9a7@gmail.com>
2019-11-08 13:14:35 -0700

    Added html to pdf change

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
 const axios = require("axios");
-const electron = require("electron");
-const generateHTML = require("./generateHTML")
-
-const writeFileAsync = util.promisify(fs.writeFile);
+const pdf = require("html-pdf");
+const generateHTML = require("./generateHTML");
 
 const questions = [
     {
@@ -28,9 +26,18 @@ function getUserStars(url) {
     return axios.get(url);
   }
 
+  function createPdf(file) {
+      const html = fs.readFileSync(file, 'utf-8');
+      const options = { format: 'Letter'};
+
+      pdf.create(html, options).toFile('./index.pdf', function(err, res) {
+          if (err) return console.log(err);
+      })
+  }
+
 async function promptUser() {
     const userInput = await inquirer.prompt(questions);
-    console.log(userInput);
+
     const queryUser = `https://api.github.com/users/${userInput.username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
 
     const queryStar = `https://api.github.com/users/${userInput.username}/repos?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&per_page=100`
@@ -38,8 +45,13 @@ async function promptUser() {
     axios.all([getUserProfile(queryUser), getUserStars(queryStar)])
     .then(axios.spread(function (profile, stars) {
         const html = generateHTML(userInput, profile, stars);
-        writeFileAsync("index.html", html);
+        fs.writeFile("index.html", html, function(err) {
+            if (err) {
+              throw err;
+            }});
     }));
+
+    createPdf('index.html');
 }
 
 promptUser();
