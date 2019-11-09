b84e0412c83cb601aaf5dd8158c46ebb758da51e
Jacob Lara <l1a9r9a7@gmail.com>
2019-11-08 20:56:06 -0700

    Fixed create pdf

--- app.js
+++ app.js
@@ -20,20 +20,20 @@ const questions = [
 
 function getUserProfile(url) {
     return axios.get(url);
-  }
-   
+}
+
 function getUserStars(url) {
     return axios.get(url);
-  }
+}
 
-  function createPdf(file) {
-      const html = fs.readFileSync(file, 'utf-8');
-      const options = { format: 'Letter'};
+function createPdf(file) {
+    const html = fs.readFileSync(file, 'utf-8');
+    const options = { format: 'Letter' };
 
-      pdf.create(html, options).toFile('./index.pdf', function(err, res) {
-          if (err) return console.log(err);
-      })
-  }
+    pdf.create(html, options).toFile('./index.pdf', function (err, res) {
+        if (err) return console.log(err);
+    })
+}
 
 async function promptUser() {
     const userInput = await inquirer.prompt(questions);
@@ -43,15 +43,15 @@ async function promptUser() {
     const queryStar = `https://api.github.com/users/${userInput.username}/repos?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&per_page=100`
 
     axios.all([getUserProfile(queryUser), getUserStars(queryStar)])
-    .then(axios.spread(function (profile, stars) {
-        const html = generateHTML(userInput, profile, stars);
-        fs.writeFile("index.html", html, function(err) {
-            if (err) {
-              throw err;
-            }});
-    }));
-
-    createPdf('index.html');
+        .then(axios.spread(function (profile, stars) {
+            const html = generateHTML(userInput, profile, stars);
+            fs.writeFile("index.html", html, function (err) {
+                if (err) {
+                    throw err;
+                }
+                createPdf('index.html');
+            });
+        }));
 }
 
 promptUser();
